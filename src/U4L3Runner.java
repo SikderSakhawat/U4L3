ff302dc8cee6aa1bc9e1aff920b3fe28d9df5c3b
unknown <BT_4N2_34@BKTECH.LOCAL>
2022-10-31 12:34:36 -0400

    Initial commit

---
+++ src/U4L3Runner.java
@@ -0,0 +1,7 @@
+public class U4L3Runner {
+    public static void main(String[] args) {
+        WhileLoopFun fun = new WhileLoopFun();
+        System.out.println(fun.factorial(20));
+        fun.printDigits(123);
+    }
+}
---
+++ src/WhileLoopFun.java
@@ -0,0 +1,24 @@
+public class WhileLoopFun {
+    public WhileLoopFun(){}
+    public double factorial(int number){
+        double factor = number;
+        double product = 1;
+        while (factor != 1){
+            product = product * (factor - 1);
+            factor = factor - 1;
+        }
+        return product * number;
+    }
+    public void printDigits(int number){
+        while(number != 0){
+            int newNum = 1;
+            int num1 = number%(newNum*10);
+            int num2 = 0;
+            num1 = num2;
+            System.out.println(num2);
+            newNum = newNum * 10;
+            
+        }
+    }
+    public int sumOf
+}
